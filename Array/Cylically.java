a6450a7aefa88c2a1d334733710190939b2e9903
Mayuresh Phalle <58583175+mayuresh18alt@users.noreply.github.com>
2024-06-26 10:03:00 +0530

    A

--- Array/Cylically.java
+++ Array/Cylically.java
@@ -1,18 +1,8 @@
 import java.util.Arrays;
 
 public class Cylically {
+
+    public static void main(String[] args){}
+
     
-    public static void main(String[] args) {
-        
-        int a[]={1,2,3,4,5};
-
-        int x=a[a.length-1];
-        for(int i=a.length-1;i>0;i--)
-        {
-            a[i]=a[i-1];
-        }
-        a[0]=x;
-
-        System.out.println(Arrays.toString(a));
-    }
 }
